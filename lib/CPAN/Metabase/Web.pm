808572f77b465a90321235b7e57d5117cc97e16e
Ricardo SIGNES <rjbs@cpan.org>
2008-04-08 18:00:33 -0400

    silence noise

--- lib/CPAN/Metabase/Web.pm
+++ lib/CPAN/Metabase/Web.pm
@@ -6,9 +6,9 @@ our $VERSION = '0.001';
 $VERSION = eval $VERSION; # convert '1.23_45' to 1.2345
 
 use Catalyst::Runtime '5.70';
-use Catalyst qw/ConfigLoader -Debug/;
+use Catalyst qw/ConfigLoader/;
 
-__PACKAGE__->setup;
 __PACKAGE__->config(name => __PACKAGE__);
+__PACKAGE__->setup;
 
 1;
