2b4cbc61e88eb831b29fcd075f115addeaa12908
Ricardo SIGNES <rjbs@cpan.org>
2008-04-08 12:10:15 -0400

    fall in line with current practices

--- lib/CPAN/Metabase/Web/Controller/Root.pm
+++ lib/CPAN/Metabase/Web/Controller/Root.pm
@@ -16,8 +16,8 @@ use Data::GUID;
 my $gateway;
 BEGIN {
    $gateway ||= CPAN::Metabase::Gateway->new({
-    analyzers => [ CPAN::Metabase::Analyzer::TestFact->new ],
-    storage   => CPAN::Metabase::Storage::Filesystem->new({
+    fact_classes => [ 'CPAN::Metabase::Fact::TestFact' ],
+    storage      => CPAN::Metabase::Storage::Filesystem->new({
       root_dir => './mb',
     }),
   });
@@ -49,16 +49,18 @@ sub dist : Chained('submit') Args(3) ActionClass('REST') {
   );
 }
 
-sub dist_POST {
+sub dist_PUT {
   my ($self, $c) = @_;
 
   $c->stash->{content} = $c->req->data->{content};
 
-  my $result = eval { $self->_gateway->handle($c->stash); 1; };
+  # XXX: In the future, this might be a queue id.  That might be a guid.  Time
+  # will tell! -- rjbs, 2008-04-08
+  my $guid = eval { $self->_gateway->handle($c->stash); };
 
-  unless ($result) {
-    my $error = $@;
-    warn $error; # XXX: we should catch and report Permission exceptions
+  unless ($guid) {
+    my $error = $@ || '(unknown error)';
+    warn $error; # XXX: we should catch and report Permission exceptions, etc
                  # -- rjbs, 2008-04-07
 
     return $self->status_bad_request($c, message => "gateway failure: $error");
@@ -66,12 +68,12 @@ sub dist_POST {
 
   return $self->status_created(
     $c,
-    location => 'http://www.google.com/',
-    entity   => { guid => 'unknown' },
+    location => '/guid/' . $guid->as_string, # XXX: uri_for or something?
+    entity   => { guid => $guid->as_string },
   );
 }
 
-BEGIN{ *dist_PUT = \&dist_POST; }
+BEGIN{ *dist_POST = \&dist_PUT; }
 
 # /guid/CC3F4AF4-0571-11DD-AA50-85A198B5225E
 #  guid 0
