24e2cca191012aa37d92cb979468eb85562d7ea7
Ricardo SIGNES <rjbs@cpan.org>
2008-04-08 08:34:17 -0400

    basic client

---
+++ lib/CPAN/Metabase/Client.pm
@@ -0,0 +1,60 @@
+use strict;
+use warnings;
+package CPAN::Metabase::Client;
+
+our $VERSION = '0.001';
+
+use HTTP::Request;
+use JSON::XS;
+use LWP::UserAgent;
+use URI;
+
+my @valid_args;
+BEGIN { @valid_args = qw(user key url) };
+use Object::Tiny @valid_args;
+
+sub new {
+  my ($class, @args) = @_;
+
+  my %args = Params::Validate::validate(
+    @args,
+    { map { $_ => 1 } @valid_args }
+  );
+
+  my $self = bless \%args, $class;
+
+  return $self;
+}   
+
+sub user_agent {
+  $_[0]->{_ua} ||= LWP::UserAgent->new;
+}
+
+sub submit_fact {
+  my ($self, $fact) = @_;
+
+  my $path = sprintf 'submit/dist/%s/%s/%s',
+    $fact->dist_author,
+    $fact->dist_file,
+    $fact->type;
+
+  my $req_uri = URI->new($path)->abs($self->url);
+
+  my $req = HTTP::Request->new(
+    PUT => $req_uri,
+    [
+      'Content-type' => 'text/x-json',
+      'Accept'       => 'text/x-json',
+    ],
+    JSON::XS->new->encode({
+      version => $fact->schema_version,
+      content => $fact->content_as_string,
+    }),
+  );
+
+  # Is it reasonable to return an HTTP::Response?  I don't know.  For now,
+  # let's say yes.
+  my $response = $self->user_agent->request($req);
+}
+
+1;
