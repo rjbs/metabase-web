c1855f6988a588f552a4fa2e82ebf306ca699582
David Golden <dagolden@cpan.org>
2009-06-25 00:09:40 -0400

    fixed up quickstart if running with -Mylib

--- example/cpan-testers/quickstart.pl
+++ example/cpan-testers/quickstart.pl
@@ -3,12 +3,14 @@ use strict;
 use warnings;
 use Path::Class;
 use File::Temp;
+use Config;
 
 my $dir = shift @ARGV;
 $dir ||= File::Temp::tempdir( 'cpan-testers-XXXX', TMPDIR => 1, CLEANUP => 1 );
 die "'$dir' is not a directory\n" unless -d $dir;
 
 my $bindir = file($0)->dir;
-system( $^X, $bindir->file('generate_config.pl'), $dir );
-system( $^X, $bindir->file('cpan-testers-metabase.pl'), '-C', file($dir,'config.json'), '-d' );
+my @mylib = $INC{'ylib.pm'} ? ('-Mylib') : ();
+system( $^X, @mylib, $bindir->file('generate_config.pl'), $dir );
+system( $^X, @mylib, $bindir->file('cpan-testers-metabase.pl'), '-C', file($dir,'config.json'), '-d' );
 
