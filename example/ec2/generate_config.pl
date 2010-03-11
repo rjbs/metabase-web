28c5e602c7e1980125067bd69ce8e3c2923cbd8f
David Golden <dagolden@cpan.org>
2010-03-11 12:31:49 +0000

    parameterize config generator

--- example/ec2/generate_config.pl
+++ example/ec2/generate_config.pl
@@ -4,17 +4,26 @@ use warnings;
 use Path::Class;
 use JSON;
 use Config::Tiny;
+use Getopt::Lucid qw/:all/;
 
-my ($filename) = @ARGV;
-die "Usage: $0 <filename>\n" unless $filename;
+my $opts = Getopt::Lucid->getopt([
+  Param("config|C")->required,
+  Param("bucket|b")->required,
+  Param("namespace|n")->required,
+]);
+
 
 my $config = {
   'Model::Metabase' => {
     class => 'CPAN::Testers::Metabase::AWS',
+    args => {
+      bucket => $opts->get_bucket,
+      namespace => $opts->get_namespace,
+    },        
   }
 };
 
-my $config_file = file($filename);
+my $config_file = file($opts->get_config);
 my $fh = $config_file->openw;
 print { $fh } JSON->new->encode($config);
 close $fh;
